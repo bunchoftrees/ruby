7d5db4a8aa11854c1c812e867efa42f86d3509ab
Forrest Hartley <forrest@forrestbear.org>
2020-11-09 02:18:01 -0500

    how to add to an array

---
+++ ruby/collections/array_addition.rb
@@ -0,0 +1,7 @@
+grocery_list = ["milk", "eggs", "bread"]
+grocery_list << "carrots"
+grocery_list.push("potatoes")
+grocery_list.unshift("celery")
+grocery_list += ["ice cream", "pie"]
+
+puts grocery_list.inspect
