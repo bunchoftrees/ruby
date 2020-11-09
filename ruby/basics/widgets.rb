a329cb3839d888b7a40c09f399756fb6c0b0ed03
Forrest Hartley <forrest@forrestbear.org>
2020-11-09 01:26:45 -0500

    discount feature

--- ruby/basics/widgets.rb
+++ ruby/basics/widgets.rb
@@ -3,8 +3,21 @@ def ask(question)
     gets.chomp
 end
 
+def price(quantity)
+    if quantity >= 100
+        price_per_unit = 8
+    elsif quantity >= 50
+        price_per_unit = 9
+    else
+        price_per_unit =10
+    end
+    quantity * price_per_unit
+end
+
+
 puts "Welcome to the Widgets Store."
 answer = ask("How many widgets are you ordering?")
 number = answer.to_i
-puts "For #{answer} widget(s), your total is $#{number * 10}."
+total = price(number)
+puts "For #{answer} widget(s), your total is $#{total}."
 
