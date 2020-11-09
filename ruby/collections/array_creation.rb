85be689a12d5b2b215d9ea712e50bd45adbfced7
Forrest Hartley <forrest@forrestbear.org>
2020-11-09 02:17:30 -0500

    how to use string interpolation in array creation

---
+++ ruby/collections/array_creation.rb
@@ -0,0 +1,8 @@
+# percent strings: https://docs.ruby-lang.org/en/2.0.0/syntax/literals_rdoc.html#label-Percent+Strings
+item = "milk"
+grocery_array = %W[#{item} eggs bread]
+
+puts grocery_array.inspect
+
+
+
