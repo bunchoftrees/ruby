a93f2b17f707662df3f61f11981b37bb1bb34b68
Forrest Hartley <forrest@forrestbear.org>
2020-11-13 03:02:31 -0500

    contact list script

--- ruby/loops/contact_list.rb
+++ ruby/loops/contact_list.rb
@@ -1,13 +1,42 @@
 # frozen_string_literal: true
 
-contact_list = []
-
 def ask(question, kind = 'string')
-  puts "#{question} "
+  print "#{question} "
   answer = gets.chomp
   answer = answer.to_i if kind == 'number'
   answer
 end
 
-answer = ask('What is your name?')
-puts answer
+def add_contact
+  contact = { 'name' => '', 'phone_numbers' => [] }
+  contact['name'] = ask('What is the person\'s name?')
+  answer = ''
+  while answer != 'n'
+    answer = ask('Do you want to add a phone number? (y/n)')
+    if answer == 'y'
+      phone = ask('Enter a phone number:')
+      contact['phone_numbers'].push(phone)
+    end
+  end
+  contact
+end
+
+contact_list = []
+
+answer = ''
+while answer != 'n'
+  contact_list.push(add_contact)
+  answer = ask('Add another? (y/n)')
+end
+
+puts '---'
+
+contact_list.each do |contact|
+  puts "Name: #{contact['name']}"
+  if contact['phone_numbers'].size.positive?
+    contact['phone_numbers'].each do |phone_number|
+      puts "Phone: #{phone_number}"
+    end
+  end
+  puts "-----\n"
+end
